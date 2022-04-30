5ee601ea3a2a003074f94a45ec17883bd9255d01
Felipe de Oliveira <felipe@deoliveira.dev>
2022-04-30 13:20:55 +0200

    Ready EC20 token
    Signed-off-by: Felipe de Oliveira <felipe@deoliveira.dev>

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -8,9 +8,11 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-    
+    mapping(address => mapping(address => uint256)) public allowance;
+
     // Events
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
     constructor() {
         totalSupply = 1000000 * (10 ** decimals);
@@ -20,10 +22,33 @@ contract Token {
     // Send token
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, 'VM Exception while processing transaction: revert.');
-        require(_to != address(0));
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
         return true;
+    }
+
+    // Transfers from
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, 'VM Exception while processing transaction: revert.');
+        require(allowance[_from][msg.sender] >= _value) ;
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    // Approve tokens
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Internal transfer function
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_from != address(0));
+        require(_to != address(0));
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,11 @@
+import { before } from 'lodash'
 import { tokens, EVM_REVERT } from './helpers'
 const Token = artifacts.require('./Token')
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', ([deployer, receiver]) => {
+contract('Token', ([deployer, receiver, exchange]) => {
     let token
     const name = 'PacheCoin'
     const symbol = 'PCH'
@@ -42,7 +43,7 @@ contract('Token', ([deployer, receiver]) => {
         })
     })
 
-    describe('sending tokens', () => {
+    describe('delegated tokens transfer', () => {
         let amount
         let result
 
@@ -64,9 +65,9 @@ contract('Token', ([deployer, receiver]) => {
                 const log = result.logs[0]
                 const event = log.args
                 log.event.should.eq('Transfer')
-                event.from.toString().should.equal(deployer, 'from is correct')
-                event.to.toString().should.equal(receiver, 'to is correct')
-                event.value.toString().should.equal(amount.toString(), 'value is correct')
+                event._from.toString().should.equal(deployer, '_from is correct')
+                event._to.toString().should.equal(receiver, '_to is correct')
+                event._value.toString().should.equal(amount.toString(), '_value is correct')
             })
         })
 
@@ -83,6 +84,96 @@ contract('Token', ([deployer, receiver]) => {
             })
             it('rejects invalid receivers', async() => {
                 await token.transfer(0x00, amount, {from: deployer}).should.be.rejected
+            })
+        })
+    })
+
+    describe('approving tokens', () => {
+        let amount
+        let result
+        beforeEach(async () => {
+            amount = tokens(100)
+            result = await token.approve(exchange, amount, {from: deployer})
+        })
+
+        describe('success', () => {
+            it('allocates an allowance for delegated token spending on exchange', async () => {
+                const allowance = await token.allowance(deployer, exchange)
+                allowance.toString().should.equal(amount.toString())
+            })
+            it ('emits a Approval event', () => {
+                const log = result.logs[0]
+                const event = log.args
+                log.event.should.eq('Approval')
+                event._owner.toString().should.equal(deployer, '_onwer is correct')
+                event._spender.toString().should.equal(exchange, '_spender is correct')
+                event._value.toString().should.equal(amount.toString(), '_value is correct')
+            })
+        })
+        describe('failure', () => {
+            it('rejects invalid spender', async() => {
+                await token.approve(0x00, amount, {from: deployer}).should.be.rejected
+            })
+        })
+    })
+
+    describe('transfer from tokens', () => {
+        let amount
+        let approveResult
+        let transferResult
+        beforeEach(async () => {
+            amount = tokens(100)
+            approveResult = await token.approve(exchange, amount, {from: deployer})
+        })
+
+        describe('success', () => {
+            beforeEach(async () => {
+                transferResult = await token.transferFrom(deployer, receiver, amount, {from: exchange})
+            })
+            it ('emits a Approval event', () => {
+                const log = approveResult.logs[0]
+                const event = log.args
+                log.event.should.eq('Approval')
+                event._owner.toString().should.equal(deployer, '_onwer is correct')
+                event._spender.toString().should.equal(exchange, '_spender is correct')
+                event._value.toString().should.equal(amount.toString(), '_value is correct')
+            })
+            it('emits a Transfer event', () => {
+                const log = transferResult.logs[0]
+                const event = log.args
+                log.event.should.eq('Transfer')
+                event._from.toString().should.equal(deployer, '_from is correct')
+                event._to.toString().should.equal(receiver, '_to is correct')
+                event._value.toString().should.equal(amount.toString(), '_value is correct')
+            })
+            it('transfer from deployer to receiver', async () => {
+                let balanceOf = await token.balanceOf(receiver)
+                balanceOf.toString().should.equal(amount.toString())
+                balanceOf = await token.balanceOf(deployer)
+                balanceOf.toString().should.equal(tokens(999900).toString())
+            })
+            it('resets the allowance', async () => {
+                const allowance = await token.allowance(deployer, exchange)
+                allowance.toString().should.equal('0')
+            })
+        })
+
+        describe('failure', () => {
+            it('reject invalid _from', async () => {
+                await token.transferFrom(0x00, receiver, amount, {from: exchange}).should.be.rejected
+            })
+            it('reject invalid _to', async () => {
+                await token.transferFrom(deployer, 0x00, amount, {from: exchange}).should.be.rejected
+            })
+            it('rejects insufficient balances', async () => {
+                let invalidAmount
+                // Try to transfer more tokens than you have
+                invalidAmount = tokens(100000000) // 100 million - greater than total supply
+                await token.transferFrom(deployer, receiver, invalidAmount, {from: exchange}).should.be.rejectedWith(EVM_REVERT)
+            
+                // Try to transfer tokens from an empty account
+                invalidAmount = tokens(10) // receiver has no tokens
+                await token.transferFrom(receiver, deployer, invalidAmount, {from: exchange}).should.be.rejectedWith(EVM_REVERT) 
             })
         })
     })
